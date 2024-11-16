1c48d91432a67579882f3fea5f91c7e37defe554
Rahul Soni <rahulsoni2225@gmail.com>
2024-11-16 22:12:40 +0530

    feat: add migration

---
+++ migrations/001_create_students_table.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS students (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    age INT NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    course VARCHAR(100) NOT NULL
+);
+
